a0b0ac44b6dfc714c68881bf23bf51c8768a2e91
llllera <144013057+llllera@users.noreply.github.com>
2024-01-08 19:20:19 +0300

    Update form.js

--- zadanie9/form.js
+++ zadanie9/form.js
@@ -1,39 +1,98 @@
+// window.addEventListener('DOMContentLoaded', function(){
+//     let name = document.getElementById("forName");
+//     let email = document.getElementById("forEmail");
+//     name.value = localStorage.getItem('forName');
+//     name.addEventListener("change", event => {
+//         localStorage.setItem('forName', name.value);
+//     });
 
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+//     email.value = localStorage.getItem('forEmail');
+//     email.addEventListener("change", event => {
+//         localStorage.setItem('forEmail', email.value);
+//     });
+// })
+// document.addEventListener('DOMContentLoaded', function(){
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
 
-    async function formSend(e){
+    
 
-        e.preventDefault();
+//     async function formSend(e){
 
-        let error = formValidate(form);
+//         e.preventDefault();
 
-        let formData =  new FormData(form);
+//         let error = formValidate(form);
 
-        if(error===0){
-            form.classList.add('_sending');
-            let responce = await fetch('https://formcarry.com/s/gf9XxUK37', {
-                method: 'POST',
-                body: formData
-            })
-            if(responce.ok){
-                alert('Успешно');
-            }else{
-                alert('Что-то пошло не так..');
-            }
+//         let formData =  new FormData(form);
+
+//         if(error===0){
+//             form.classList.add('_sending');
+//             let responce = await fetch('https://formcarry.com/s/gf9XxUK37', {
+//                 method: 'POST',
+//                 body: formData
+//             })
+//             if(responce.ok){
+//                 alert('Успешно');
+//             }else{
+//                 alert('Что-то пошло не так..');
+//             }
             
-        }else{
-            alert('Дайте согалсие на обработку данных');
-        }
-    }
+//         }else{
+//             alert('Дайте согалсие на обработку данных');
+//         }
+//     }
 
-    function formValidate(form){
-        let error = 0;
-        let input = document.getElementById("forAgreement");
-        if(input.getAttribute("type")==="checkbox" && input.checked===false){
-            error++;
+//     function formValidate(form){
+//         let error = 0;
+//         let input = document.getElementById("forAgreement");
+//         if(input.getAttribute("type")==="checkbox" && input.checked===false){
+//             error++;
+//         }
+//         return error;
+//     }
+// })
+window.addEventListener("DOMContentLoaded", function () {
+    let name = document.getElementById("forName");
+    let email = document.getElementById("forEmail");
+
+    name.value = localStorage.getItem('forName');
+    name.addEventListener("change", event => {
+        localStorage.setItem('forName', name.value);
+    });
+
+    email.value = localStorage.getItem('forEmail');
+    email.addEventListener("change", event => {
+        localStorage.setItem('forEmail', email.value);
+    });
+
+    let check = document.getElementById("forAgreement");
+    let form = document.getElementById("formm");
+    form.addEventListener("submit", event => {
+        if (check.checked == true) {
+            fetch('https://formcarry.com/s/gf9XxUK37', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+                body: JSON.stringify({
+                    formName: name.value,
+                    formEmail: email.value,
+                })
+            })
+                .then(response => console.log(response))
+                .then((data) => {
+                    alert("Success!");
+                    localStorage.removeItem('forName');
+                    localStorage.removeItem('forEmail');
+            
+                    name.value = localStorage.getItem('forName');
+                    email.value = localStorage.getItem('forEmail');
+                  
+                })
+                .catch((error) => { alert("Error"); });
+            event.preventDefault();
         }
-        return error;
-    }
-})
+        else {
+            alert("Дайте свое согласие на отправку");
+            event.preventDefault();
+        }
+    })
+});
